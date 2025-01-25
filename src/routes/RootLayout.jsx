0e520e3dbc700985a7c458a30e3c3ea3ef6c3f04
STEPHEN286 <kwofie286@gmail.com>
2025-01-25 00:31:23 +0000

    adding product categories sidebar to rootlayout

--- src/routes/RootLayout.jsx
+++ src/routes/RootLayout.jsx
@@ -3,19 +3,20 @@ import Header from "../components/header/Header";
 import { Outlet } from "react-router";
 
 import { FooterComponent } from "../components/footer/Footer";
+import Sidebar from "../components/sidebar/Sidebar";
 
 export default function RootLayout() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen  ">
       <Header />
-      <main className=" mx-auto ">
-        {/* <div className="flex">
-          <Sidebar /> */}
+      <main className="mx-auto max-w-7xl px-4 sm:px-6 mb-4 lg:px-8">
+         <div className="flex md:gap-4">
+          <Sidebar /> 
         <div className="md:flex-1 flex-col max-sm:w-screen  ">
           <Outlet />
         </div>
-        {/* </div> */}
-      </main>
+        </div>
+      </main > 
       <FooterComponent />
     </div>
   );
